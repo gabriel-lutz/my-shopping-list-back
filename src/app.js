c0c616bb644511562d9d891a23a09be9dcfe3d4e
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-07-12 14:46:36 -0300

    Added get/items method

--- src/app.js
+++ src/app.js
@@ -7,4 +7,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/items", async (req,res)=>{
+    try{
+        const query = await connection.query(`
+            SELECT * FROM list
+            `)
+        res.send(query.rows)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
 export default app;
