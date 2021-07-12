216fc4d49218a7b81a047efc23b4c1aba6f1cf61
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-07-12 16:00:48 -0300

    added post/items methos to insert items into database

--- src/app.js
+++ src/app.js
@@ -7,6 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/items", async(req,res) =>{
+    try{
+        const item = req.body["text"];
+
+        const query = await connection.query(`
+        INSERT INTO list
+        (item) VALUES ($1)
+        `, [item])
+        res.sendStatus(200)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
 app.get("/items", async (req,res)=>{
     try{
         const query = await connection.query(`
